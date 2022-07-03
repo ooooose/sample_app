ae1871c64e549c83b23f20680a1299907d8af05b
ooooose <yuuki.oose1990@gmail.com>
2022-07-03 07:15:37 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,10 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   # 現在のユーザーをログアウトする
   def log_out
@@ -38,5 +42,14 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
   
 end
