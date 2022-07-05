073b6cc26e5acc6416b0ad8589f61b4c50bb67fc
ooooose <yuuki.oose1990@gmail.com>
2022-07-05 11:31:10 +0000

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "Invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
@@ -14,15 +18,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template "users/new"
   end
   
-  test "Valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
-    assert_difference "User.count", 1 do
-      post users_path, params: {user: {name: "Example User",
-      email: "user@example.com", password: "password",
-        password_confirmation: "password" }}
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # 有効化していない状態でログインしてみる
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    assert_template "users/show"
+    assert_template 'users/show'
     assert is_logged_in?
   end
   
